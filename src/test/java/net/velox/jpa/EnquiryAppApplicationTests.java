603a9c519f93c7800e7770e482953dd4158f0545
mahalban <albanmahmut@gmail.com>
2020-07-05 20:06:25 +0200

    created spring project

---
+++ src/test/java/net/velox/jpa/EnquiryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.velox.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnquiryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
